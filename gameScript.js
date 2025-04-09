eb9a0e808b821bf351aee37b5246f8dc21368c3c
Aethelryth <piggytron2000@gmail.com>
2025-04-09 17:22:27 +0800

    Updated gameScript to prepare for future changes

--- gameScript.js
+++ gameScript.js
@@ -9,16 +9,13 @@ function randomFromArrayExcludeArray(array_to_source, array_to_exclude) {
         console.log("FATAL: FOUND ALL OF ", array_to_source, " IN ", array_to_exclude);
         endGame();
     }
-    let newItem = randomFromArray(array_to_source);
+    var newItem = randomFromArray(array_to_source);
     while (array_to_exclude.includes(newItem)) {
         newItem = randomFromArray(array_to_source);
     }
     return newItem
 }
 
-var gamePieces = [];
-var numGamePieces = 10;
-var trianglePiece;
 
 function startGame() {
     myGameArea.start();
@@ -29,8 +26,10 @@ function startGame() {
     SCNComponentManager.addShape();
     SCNComponentManager.addShape();
     // TODO add SCNComponent manager start
+    ting = new buttonRect(50, 50, "red", "hi", 50, 50);
 }
 
+// Game area canvas and initialiser
 var myGameArea = {
     canvas: document.createElement("canvas"),
     start: function () {
@@ -38,7 +37,7 @@ var myGameArea = {
         this.canvas.height = window.innerHeight * 0.80;
         this.context = this.canvas.getContext("2d");
         this.canvas.style.marginBottom = "60px";
-        let fontsize = this.canvas.width / 25;
+        var fontsize = this.canvas.width / 25;
         this.font = fontsize + "px Verdana";
         document.getElementById("content").appendChild(this.canvas);
         this.interval = setInterval(updateGameArea, 20);
@@ -51,7 +50,7 @@ var myGameArea = {
     }
 }
 
-
+// Helper class, used to create and manage each seen component with a shape, colour and number
 class shapeColourNumberComponent {
     constructor(shape, shapeColor, number, numberColor, x, y, size) {
         this.shape = shape;
@@ -66,7 +65,7 @@ class shapeColourNumberComponent {
             this.y = y;
         }
         this.update = function () {
-            let ctx = myGameArea.context;
+            var ctx = myGameArea.context;
             ctx.fillStyle = this.shapeColor;
             // Draw the shape, TODO all shapes
             ctx.beginPath();
@@ -81,13 +80,13 @@ class shapeColourNumberComponent {
             ctx.fillStyle = this.numberColor;
             ctx.strokeStyle = "black";
             ctx.font = myGameArea.font;
-            let textWidth = ctx.measureText(this.number).width;
+            var textWidth = ctx.measureText(this.number).width;
             ctx.strokeText(this.number, this.x + Math.floor(this.size / 2) - Math.floor(textWidth / 2), this.y + Math.floor(this.size * 0.9));
             ctx.fillText(this.number, this.x + Math.floor(this.size / 2) - Math.floor(textWidth / 2), this.y + Math.floor(this.size * 0.9));
         }
     }
 }
-
+// Manager of shapeColourNumber components
 var SCNComponentManager = {
     start: function (number_of_starting_shapes) {
         this.num_shapes = number_of_starting_shapes;
@@ -115,11 +114,11 @@ var SCNComponentManager = {
             console.log("Shapes limit exceeded, exiting...");
             endGame();
         }
-        let newShape = randomFromArrayExcludeArray(this.possibleShapes, this.usedShapes);
+        var newShape = randomFromArrayExcludeArray(this.possibleShapes, this.usedShapes);
         newShape = "Triangle";
-        let newNum = randomFromArrayExcludeArray(this.possibleNums, this.usedNums);
-        let newShapeCol = randomFromArrayExcludeArray(this.possibleShapeCols, this.usedShapeCols);
-        let newNumCol = randomFromArrayExcludeArray(this.possibleNumCols, this.usedNumCols);
+        var newNum = randomFromArrayExcludeArray(this.possibleNums, this.usedNums);
+        var newShapeCol = randomFromArrayExcludeArray(this.possibleShapeCols, this.usedShapeCols);
+        var newNumCol = randomFromArrayExcludeArray(this.possibleNumCols, this.usedNumCols);
         this.shapes.push(new shapeColourNumberComponent(newShape, newShapeCol, newNum, newNumCol, 0, 0, this.shapeSize));
         this.usedShapes.push(newShape);
         this.usedShapeCols.push(newShapeCol);
@@ -130,15 +129,15 @@ var SCNComponentManager = {
         // Shapes are x-positioned by the following rules:
         // 10% of the canvas on each side is blank (total 20%)
         // the rest of the space is divided into 80% / maximumShapes
-        let increment = Math.floor(myGameArea.canvas.width * 0.70) / (this.shapes.length-1);
-        let leftMost = Math.floor(myGameArea.canvas.width * 0.15);
-        let yPos = Math.floor(myGameArea.canvas.height * 0.45);
-        for (let index = 0; index < this.shapes.length; index++) {
+        var increment = Math.floor(myGameArea.canvas.width * 0.70) / (this.shapes.length - 1);
+        var leftMost = Math.floor(myGameArea.canvas.width * 0.15);
+        var yPos = Math.floor(myGameArea.canvas.height * 0.45);
+        for (var index = 0; index < this.shapes.length; index++) {
             this.shapes[index].move(leftMost + increment * index, yPos);
             this.shapes[index].update();
         }
     },
-    removeShapes: function () {
+    clearSelf: function () {
         this.shapes = [];
         this.usedShapes = [];
         this.usedCols = [];
@@ -147,9 +146,64 @@ var SCNComponentManager = {
     }
 }
 
+// Clickable rectangle
+class buttonRect {
+    constructor(width, height, color, text, x, y) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.text = text;
+        this.x = x;
+        this.y = y;
+        this.update = function () {
+            var ctx = myGameArea.context;
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+            ctx.fillStyle = this.numberColor;
+            ctx.strokeStyle = "black";
+            ctx.font = myGameArea.font;
+            var textWidth = ctx.measureText(this.text).width;
+            ctx.strokeText(this.text, this.x + Math.floor(this.width / 2) - Math.floor(textWidth / 2), this.y + Math.floor(this.height * 0.9));
+            ctx.fillText(this.text, this.x + Math.floor(this.height / 2) - Math.floor(textWidth / 2), this.y + Math.floor(this.height * 0.9));
+        };
+        this.clicked = function () {
+            var myleft = this.x;
+            var myright = this.x + (this.width);
+            var mytop = this.y;
+            var mybottom = this.y + (this.height);
+            var clicked = true;
+            if ((mybottom < myGameArea.y) || (mytop > myGameArea.y) || (myright < myGameArea.x) || (myleft > myGameArea.x)) {
+                clicked = false;
+            }
+            if (clicked == true){
+                console.log("clicked");
+            }
+            return clicked;
+        };
+    }
+}
+
+// TODO make button manager
+var buttonManager = {
+    start: function () {
+
+    }
+}
+
+// TODO create game state:
+// Game states are start, end, showing colnums, asking for answer, reward. Score is always shown at the top.
+// Start is a button in the middle of the screen, leads to showing colnums
+// Showing colnums is self descriptive, either button or timeout to answer input
+// Answer input goes to reward screen
+// Reward screen shows correct/incorrect, goes to showing colnums or end screen (2 buttons)
+var BTGame = {
+    state: "Await",
+}
+
 function updateGameArea() {
     myGameArea.clear();
     SCNComponentManager.arrangeShapes();
+    ting.update();
 }
 
 function endGame() {

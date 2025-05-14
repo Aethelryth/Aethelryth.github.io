094e93aee66dcb8663d67a8736019f5be4bc5bbc
Aethelryth <piggytron2000@gmail.com>
2025-05-14 10:46:18 +0800

    Scope pivot to portfolio website, removed and modified files to start work on this

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-// Function to execute code based on the hash
-function handleHashChange() {
-    // Get the current hash from the URL 
-    const hash = window.location.hash;
-    if (hash != "#game") {
-        endGame();
-    }
-    if (hash === "#home") {
-        document.getElementById("content").innerHTML = "<p>I hope to have something cool here to show someday!</p>";
-    } else if (hash === "#index") {
-        document.getElementById("content").innerHTML = "<p>I want so many things they'll have to be organised...</p>";
-    } else if (hash === "#game") {
-        document.getElementById("content").innerHTML = "<p>Reload the page for a fun visual of my current progress on this brain trainer game!</p>";
-        startGame();
-    } else {
-        console.log("No specific hash or unknown hash", hash);    // Debug output for unknown hashes
-        window.location.href = "#home";
-    }
+// Ensure the window is large enough
+if (window.innerWidth < 800 || windowHeight < 500){
+    document.body.innerHTML = "<p>This website is meant for bigger screens.<\p>"
 }
-
-window.onload = handleHashChange;
-window.addEventListener('hashchange', handleHashChange);
